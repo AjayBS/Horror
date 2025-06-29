eae5b6753588b42a67696a2ec5b48c8921c794ce
AJAYBS <asathish@ea.com>
2025-06-29 17:06:16 -0600

    Added mouse input to move the characte

--- Source/HorrorScare/Private/Player/HG_PlayerController.cpp
+++ Source/HorrorScare/Private/Player/HG_PlayerController.cpp
@@ -3,3 +3,44 @@
 
 #include "Player/HG_PlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+#include "GameFramework/Character.h"
+
+void AHG_PlayerController::BeginPlay()
+{
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (Subsystem)
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 1);
+	}
+}
+
+void AHG_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		// Looking
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AHG_PlayerController::Look);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+	}
+}
+
+void AHG_PlayerController::Look(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (GetCharacter() != nullptr)
+	{
+		// add yaw and pitch input to controller
+		GetCharacter()->AddControllerYawInput(LookAxisVector.X);
+		GetCharacter()->AddControllerPitchInput(-LookAxisVector.Y);
+	}
+}
--- Source/HorrorScare/Public/Player/HG_PlayerController.h
+++ Source/HorrorScare/Public/Player/HG_PlayerController.h
@@ -6,6 +6,10 @@
 #include "GameFramework/PlayerController.h"
 #include "HG_PlayerController.generated.h"
 
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
+
 /**
  * 
  */
@@ -13,5 +17,21 @@ UCLASS()
 class HORRORSCARE_API AHG_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+public:
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	// Sets default values for this controller's properties
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
 	
 };
