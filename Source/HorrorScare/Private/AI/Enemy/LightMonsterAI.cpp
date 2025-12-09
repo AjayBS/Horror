772ab35d375009022f0ce657deba9473fbcc265c
AJAYBS <bsajay.22@gmail.com>
2025-12-08 21:49:31 -0500

    Light enemy teleports from one position to another

---
+++ Source/HorrorScare/Private/AI/Enemy/LightMonsterAI.cpp
@@ -0,0 +1,21 @@
+// Confused Minds Inc.
+
+
+#include "AI/Enemy/LightMonsterAI.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+ALightMonsterAI::ALightMonsterAI()
+{
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->bUseControllerDesiredRotation = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 180.f, 0.f);
+	GetCharacterMovement()->MaxWalkSpeed = 300.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 1000.f;
+}
---
+++ Source/HorrorScare/Public/AI/Enemy/LightMonsterAI.h
@@ -0,0 +1,19 @@
+// Confused Minds Inc.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/Enemy/AIEnemyBase.h"
+#include "LightMonsterAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORSCARE_API ALightMonsterAI : public AAIEnemyBase
+{
+	GENERATED_BODY()
+	
+public:
+	ALightMonsterAI();
+};
